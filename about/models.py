5797c084e64992f120e26e83009b37e9f20c3481
Wai Yi <wongwaverley@gmail.com>
2024-12-28 00:09:01 +0000

    add model to models.py in about app

--- about/models.py
+++ about/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class FeedbackForm(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
