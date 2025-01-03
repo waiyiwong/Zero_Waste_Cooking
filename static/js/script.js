2a4a794e1b53816cde4a9a1c767507a0e8ade3d6
Wai Yi <wongwaverley@gmail.com>
2025-01-03 18:49:49 +0000

    style index and about.html and add animations and js to index.html

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElementsLeft = document.querySelectorAll('.scroll-animation-left');
+hiddenElementsLeft.forEach((el) => observer.observe(el));
+
+const hiddenElementsRight = document.querySelectorAll('.scroll-animation-right');
+hiddenElementsRight.forEach((el) => observer.observe(el));
